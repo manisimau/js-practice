e02a14c46845274f7901ab3d78cec02448e13cbb
manisimau <mikita.anisimau@gmail.com>
2025-10-27 20:37:47 +0100

    Fourth commit: Lesson 02 - Conditions

---
+++ lesson-02-conditions/script.js
@@ -0,0 +1,9 @@
+let age = 21;
+let hasDrivingLicense = true;
+if (age >= 18 && hasDrivingLicense) {
+    console.log("You can drive");
+} else if (age < 18) {
+    console.log("You are not old enough to drive");
+} else {
+    console.log("You need a driving license to drive");
+}
