72963427d0c66163f9e574f7121cf298c9731f0c
manisimau <mikita.anisimau@gmail.com>
2025-10-27 20:46:49 +0100

    Fifth commit: Lesson 02 - Conditions updated

--- lesson-02-conditions/script.js
+++ lesson-02-conditions/script.js
@@ -1,9 +1,14 @@
-let age = 21;
+let age = 15;
 let hasDrivingLicense = true;
-if (age >= 18 && hasDrivingLicense) {
+let hasCar = false;
+let hasParrentPermission = true;
+
+if (age >= 18 && hasDrivingLicense && hasCar ) {
     console.log("You can drive");
-} else if (age < 18) {
-    console.log("You are not old enough to drive");
-} else {
-    console.log("You need a driving license to drive");
+} else if (age >= 18 && hasDrivingLicense && !hasCar) {
+    console.log("You can drive, but you don't have a car.");
+} else if (age >= 16 && !hasDrivingLicense && hasParrentPermission) {
+    console.log("You can drive with parental permission.");
+} else if (age < 16 || !hasCar && !hasDrivingLicense) {
+    console.log("You cannot drive.");
 }
